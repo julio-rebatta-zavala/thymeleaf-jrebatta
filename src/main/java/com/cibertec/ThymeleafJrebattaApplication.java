f1c1d22ea308d9291dc9d7d163fed8b9d59e011e
julio-rebatta-zavala <65001342+julio-rebatta-zavala@users.noreply.github.com>
2025-07-04 20:42:43 -0500

    Add files via upload

---
+++ src/main/java/com/cibertec/ThymeleafJrebattaApplication.java
@@ -0,0 +1,13 @@
+package com.cibertec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafJrebattaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleafJrebattaApplication.class, args);
+	}
+
+}
